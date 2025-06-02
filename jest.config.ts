e135d8332c371d3584654ed4dfb493cab0db5c30
Subham Thapa <subhamits4.2@gmail.com>
2025-06-02 18:22:16 +0545

    build: added coverage to dockerignore

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
     "!src/**/*.test.{ts,tsx}",
     "!src/**/superTest.{ts,tsx}",
     "!src/**/main.{ts,tsx}",
+    "!src/**/d.{ts,tsx}",
     "!/node_modules",
   ],
   transform: {
