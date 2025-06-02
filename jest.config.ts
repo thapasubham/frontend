e264537f2673256c388aca0ccce51f872e534321
Subham Thapa <subhamits4.2@gmail.com>
2025-06-02 17:58:05 +0545

    build: added testing with jest

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageProvider: "v8",
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/superTest.{ts,tsx}",
+    "!src/**/main.{ts,tsx}",
+    "!/node_modules",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.app.json",
+      },
+    ],
+  },
+};
+
+export default config;
